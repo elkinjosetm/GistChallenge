489cb41fdf1819e5e66b758ff7ac6863e1e7b92e
Elkin Torres <elkinjosetm@gmail.com>
2018-07-02 13:15:54 -0500

    Implement a generic button component

---
+++ src/components/button/index.js
@@ -0,0 +1,99 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { TouchableOpacity, Text } from 'react-native';
+import { isEqual } from 'lodash';
+import styles from './styles';
+
+class Button extends Component {
+	static propType = {
+		title         : PropTypes.string.isRequired,
+		activeOpacity : PropTypes.number,
+		onPress       : PropTypes.func,
+		size          : PropTypes.oneOf([
+			'default',
+			'sm',
+			'lg',
+			'xlg',
+		]),
+		color : PropTypes.oneOf([
+			'default',
+			'primary',
+			'secondary',
+			'tertiary',
+		]),
+		style : PropTypes.oneOfType([
+			PropTypes.number,
+			PropTypes.object,
+			PropTypes.array,
+		]),
+		textStyle : PropTypes.oneOfType([
+			PropTypes.number,
+			PropTypes.object,
+			PropTypes.array,
+		]),
+	};
+
+	static defaultProps = {
+		activeOpacity : 0.8,
+		size          : 'default',
+		color         : 'default',
+	};
+
+	shouldComponentUpdate = ({
+		title,
+		activeOpacity,
+		size,
+		color,
+		style,
+		textStyle,
+	}) => {
+		const lastProps = this.props;
+
+		return (
+			!isEqual(title, lastProps.title) ||
+			!isEqual(activeOpacity, lastProps.activeOpacity) ||
+			!isEqual(size, lastProps.size) ||
+			!isEqual(color, lastProps.color) ||
+			!isEqual(style, lastProps.style) ||
+			!isEqual(textStyle, lastProps.textStyle)
+		);
+	}
+
+	render() {
+		const {
+			title,
+			activeOpacity,
+			onPress,
+			size,
+			color,
+			style : buttonExtraStyle,
+			textStyle : textExtraStyle,
+		} = this.props;
+		const style = [
+			styles.button,
+			styles[`${size}SizeButton`],
+			styles[`${color}ColorButton`],
+			buttonExtraStyle,
+		];
+		const textStyle = [
+			styles.text,
+			styles[`${size}SizeText`],
+			styles[`${color}ColorText`],
+			textExtraStyle,
+		];
+
+		return (
+			<TouchableOpacity
+				activeOpacity={ activeOpacity }
+				style={ style }
+				onPress={ onPress }
+			>
+				<Text style={ textStyle }>
+					{ title }
+				</Text>
+			</TouchableOpacity>
+		);
+	}
+}
+
+export default Button;
---
+++ src/components/button/styles/index.js
@@ -0,0 +1,35 @@
+import { StyleSheet } from 'react-native';
+import { Colors, Metrics } from '@theme';
+
+export default StyleSheet.create({
+	button : {
+		alignItems        : 'center',
+		paddingVertical   : 8,
+		paddingHorizontal : 30,
+		borderRadius      : Metrics.borderRadius,
+		backgroundColor   : Colors.iron,
+	},
+
+	text : {
+		textAlign  : 'center',
+		fontSize   : Metrics.fontSize,
+		lineHeight : Metrics.fontSize,
+		color      : Colors.black,
+	},
+
+	smSizeButton  : { paddingVertical : 5, paddingHorizontal : 20 },
+	lgSizeButton  : { paddingVertical : 8, paddingHorizontal : 40 },
+	xlgSizeButton : { paddingVertical : 8, paddingHorizontal : 60 },
+
+	primaryColorButton   : { backgroundColor : Colors.primary },
+	secondaryColorButton : { backgroundColor : Colors.secondary },
+	tertiaryColorButton  : { backgroundColor : Colors.tertiary },
+
+	smSizeText  : { fontSize : 13, lineHeight : 13 },
+	lgSizeText  : { fontSize : 20, lineHeight : 20 },
+	xlgSizeText : { fontSize : 25, lineHeight : 25 },
+
+	primaryColorText   : { color : Colors.white },
+	secondaryColorText : { color : Colors.white },
+	tertiaryColorText  : { color : Colors.white },
+});

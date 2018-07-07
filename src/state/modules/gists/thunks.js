749b15b822ded6a29ab952c89625914e6c58b0ce
Elkin Torres <elkinjosetm@gmail.com>
2018-07-07 11:19:55 -0500

    Navigate to DetailsScreen on load the gist

--- src/state/modules/gists/thunks.js
+++ src/state/modules/gists/thunks.js
@@ -1,4 +1,5 @@
 import { NavigationActions } from 'react-navigation';
+import { map } from 'lodash';
 import { GistService } from '@services';
 import GlobalsActions from '@redux/globals';
 import AppActions from '@redux/app';
@@ -40,9 +41,39 @@ export const getGistById = gistId => (dispatch => {
 	dispatch(AppActions.setLoading(true));
 
 	GistService.getById(gistId)
-		.then(({ data }) => {
+		.then(({ data : rawData }) => {
+			const data = { ...rawData };
+
+			/**
+			 * Since the files in the response
+			 * is an object where every file is
+			 * a separate key, we convert it to
+			 * be an array of object, so we can
+			 * use easily throughout the app.
+			 *
+			 * Also, since a file doesn't have
+			 * and id in the response, we use
+			 * the position index as KEY so it
+			 * can be used for the list render
+			 */
+			const filesArray = map(data.files, (file, index) => ({
+				...file,
+				key : index,
+			}));
+
+			// Add filesArray to the details object
+			data.filesArray = filesArray;
+
 			// Receive gist data
 			dispatch(Actions.receiveData('details', data));
+
+			// Navigate to Details page and set the page title
+			dispatch(NavigationActions.navigate({
+				routeName : 'Details',
+				params    : {
+					title : data.description,
+				},
+			}));
 
 			// Stop loading animation
 			dispatch(AppActions.setLoading(false));

0a19d7aaddd0731fa8e4d2eeabfbe2be409c0952
Elkin Torres <elkinjosetm@gmail.com>
2018-07-07 13:23:34 -0500

    Uninstall react-native-blur

--- android/app/src/main/java/com/gistchallenge/MainApplication.java
+++ android/app/src/main/java/com/gistchallenge/MainApplication.java
@@ -3,7 +3,6 @@ package com.gistchallenge;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.cmcewen.blurview.BlurViewPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.babisoft.ReactNativeLocalization.ReactNativeLocalizationPackage;
 import com.facebook.react.ReactNativeHost;
@@ -26,7 +25,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new BlurViewPackage(),
             new VectorIconsPackage(),
             new ReactNativeLocalizationPackage()
       );
